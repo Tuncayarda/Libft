40c6de86b8d759abc1f0bbf44c904ece4e3f46bf
Tuncay Arda AYDIN <tncyard@gmail.com>
2025-05-27 04:19:43 +0300

    null check

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,17 +6,19 @@
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:18:29 by tuaydin           #+#    #+#             */
-/*   Updated: 2024/10/08 15:26:39 by tuaydin          ###   ########.fr       */
+/*   Updated: 2025/05/27 04:19:22 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(char *str)
 {
 	size_t	count;
 
 	count = 0;
+	if (!str)
+		return (count);
 	while (*str++)
 		count++;
 	return (count);
